7db1b44958c4746db0e68c7d1ae203dd595dd767
eXpire163 <github@mms2k.de>
2015-12-31 16:35:39 +0100

    Update startstream.sh

--- startstream.sh
+++ startstream.sh
@@ -1,3 +1,3 @@
-sudo service motion stop
+#sudo service motion stop
 
 sudo nice -n -3 mjpg_streamer -i "/usr/local/lib/input_uvc.so -d /dev/video0 -r 320x240 -f 15 -q 85  -n" -o "/usr/local/lib/output_http.so -n -w /usr/local/www -p 8883"
