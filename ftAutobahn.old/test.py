ebf2aca4043298bb38c930b42e8c24916637af46
Christian Wiedemann <c.wiedemann@profi-ag.de>
2017-04-22 22:17:34 +0200

    remove builds

---
+++ ftAutobahn.old/test.py
@@ -0,0 +1,33 @@
+﻿import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+port = [8,10]
+
+#port = map(int, port)
+
+def my_rising(data):
+    print "rise {} {}".format(data, GPIO.input(data))
+
+print port
+
+# setup events
+#for xx in port:
+
+
+
+for xx in range(len(port)):
+    print(xx,port[xx])
+    print("setup: ".format(int(xx)))
+    GPIO.setup(port[xx], GPIO.IN)
+    GPIO.add_event_detect(port[xx], GPIO.BOTH, callback=my_rising, bouncetime=200)
+
+
+# remove events
+
+i =0
+while True:
+    pass
+  #print(GPIO.input(23))
+  #if (GPIO.input(port)):
+  #  i=i+1
+  #print(i)
