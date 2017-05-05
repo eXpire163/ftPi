2be4773297ac2390274d243f08ba03a265ba1b9e
Christian Wiedemann <c.wiedemann@profi-ag.de>
2017-05-05 21:02:43 +0200

    comment

--- minimop/motion.py
+++ minimop/motion.py
@@ -3,10 +3,10 @@ import paho.mqtt.client as mqtt
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("minimop/motion")
+    print("subscribing to minimop/motion")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
