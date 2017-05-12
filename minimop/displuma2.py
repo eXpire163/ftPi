18bdb2dc2ed516d859d3d383b588108e68a658fa
eXpire163 <github@mms2k.de>
2017-05-12 20:34:09 +0000

    port fix

--- minimop/displuma2.py
+++ minimop/displuma2.py
@@ -73,11 +73,10 @@ if __name__ == '__main__':
     try:
 
         # rev.1 users set port=0
-# substitute spi(device=0, port=0) below if using that interface
-serial = i2c(port=24, address=0x3C)
-
-# substitute ssd1331(...) or sh1106(...) below if using that device
-device = ssd1306(serial)
+        # substitute spi(device=0, port=0) below if using that interface
+        serial = i2c(port=1, address=0x3C)
+        # substitute ssd1331(...) or sh1106(...) below if using that device
+        device = ssd1306(serial)
 
         
         main()
