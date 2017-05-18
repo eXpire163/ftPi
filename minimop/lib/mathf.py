c33106a29661f62bef0e4b9ddc27655d5014d8f7
Christian Wiedemann <c.wiedemann@profi-ag.de>
2017-05-18 21:31:56 +0200

    mathe update

--- minimop/lib/mathf.py
+++ minimop/lib/mathf.py
@@ -10,3 +10,19 @@ class Mathf(object):
         y_axis = [xvalue, yvalue]
         result = np.interp(delta, x_axis, y_axis)
         return result
+
+    @staticmethod
+    def smoothstep(edge0, edge1, x):    
+        # Scale, bias and saturate x to 0..1 range
+        x = clamp((x - edge0)/(edge1 - edge0), 0.0, 1.0)
+        # Evaluate polynomial
+        return x*x*(3 - 2*x)
+
+
+    @staticmethod
+    def clamp(float x, float lowerlimit, float upperlimit):        
+        if (x < lowerlimit):
+            x = lowerlimit
+        if (x > upperlimit):
+            x = upperlimit
+        return x
