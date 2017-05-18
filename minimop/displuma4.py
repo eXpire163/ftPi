dce0d2adb48f4ee1d6306353a2d2f80d9a0e63fe
CWIEDEMA <christian.wiedemann@capgemini.com>
2017-05-18 15:04:54 +0200

    add emulator

--- minimop/displuma4.py
+++ minimop/displuma4.py
@@ -14,6 +14,7 @@ try:
     from luma.oled.device import ssd1306
     testenv = False
 except ImportError:
+    from luma.emulator.device import emulator
     testenv = True
 
 
@@ -55,6 +56,8 @@ if __name__ == '__main__':
             serial = i2c(port=1, address=0x3C)
             # substitute ssd1331(...) or sh1106(...) below if using that device
             device = ssd1306(serial)
+        else:
+            device = emulator(width=128, height=64, rotate=0, mode='1', transform='none', scale=6)
         
         
         main()
